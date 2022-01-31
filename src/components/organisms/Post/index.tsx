b348a51b81340a962f386646fcc1fb31cdde6d9e
Syncxv <aruldevarackal1@gmail.com>
2022-01-31 17:27:45 +0530

    idk man prolly gonna refactor this

--- src/components/organisms/Post/index.tsx
+++ src/components/organisms/Post/index.tsx
@@ -1,4 +1,6 @@
+import { ChatDots, Heart, PaperPlaneTilt } from 'phosphor-react'
 import React from 'react'
+import UserPill from '../../molecules/UserPill'
 
 import PST from './Post.module.scss'
 
@@ -9,15 +11,29 @@ interface Props {
 }
 
 const Post: React.FC<Props> = ({ title, content, username }) => {
+    const PostButton: React.FC<{ icon: any; likes?: number }> = ({ icon, likes }) => {
+        return (
+            <>
+                <div className={PST.actionButtonWrapper}>
+                    <button className={PST.actionButton}>
+                        <div className={PST.icon}>{icon}</div>
+                    </button>
+                    {likes && <span className={PST.likes}>{likes}</span>}
+                </div>
+            </>
+        )
+    }
     return (
         <>
             <div className={PST.post}>
-                <div className={PST.user}>
-                    <img className={PST.avatar} src="https://i.imgur.com/JUVFPMN.png" alt="" />
-                    <div className={PST.name}>@{username}</div>
-                </div>
+                <UserPill username="bruh" />
                 <div className={PST.postImage}>
                     <img src="https://i.imgur.com/Lte0y2v.jpeg" alt="" />
+                </div>
+                <div className={PST.actions}>
+                    <PostButton icon={<Heart color="white" size={32} />} likes={20} />
+                    <PostButton icon={<ChatDots color="white" size={32} />} likes={20} />
+                    <PostButton icon={<PaperPlaneTilt color="white" size={32} />} />
                 </div>
                 <div className={PST.allContent}>
                     <div className={PST.contentUser}>
