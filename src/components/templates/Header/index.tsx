f6b1b37c6fde180a9e110bce5daec1c21f1d83ba
Syncxv <aruldevarackal1@gmail.com>
2022-02-02 05:00:44 +0530

    header hehe TOOL TIPS TOO

--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -1,5 +1,8 @@
 import { gql, useMutation } from '@apollo/client'
+import { PlusCircle } from 'phosphor-react'
 import React from 'react'
+
+import HS from './Header.module.scss'
 
 type Props = {}
 
@@ -8,6 +11,14 @@ const UPLOAD_MUTATION = gql`
         uploadImage(file: $file)
     }
 `
+
+const HeaderItem: React.FC<{ tooltip: string }> = ({ children, tooltip }) => {
+    return (
+        <li className={`${HS.header__linkItem} tooltipLeft`} data-tooltip={tooltip}>
+            {children}
+        </li>
+    )
+}
 
 const Header: React.FC<Props> = ({}) => {
     const [uploadFile] = useMutation(UPLOAD_MUTATION, {
@@ -21,11 +32,11 @@ const Header: React.FC<Props> = ({}) => {
     }
     return (
         <>
-            <nav>
-                <ul>
-                    <li>
-                        <input onChange={handleChange} type="file" accept="image/png, image/jpeg" />
-                    </li>
+            <nav className={HS.header}>
+                <ul className={HS.header__items}>
+                    <HeaderItem tooltip="post">
+                        <PlusCircle size={32} />
+                    </HeaderItem>
                 </ul>
             </nav>
         </>
