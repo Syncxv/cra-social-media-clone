7673045c8eefa17de7e7bf6cbc934a1d8872a97b
Syncxv <aruldevarackal1@gmail.com>
2022-02-02 04:23:43 +0530

    STAY LOGGED IN on refresh gang

--- src/auth/AuthMiddlewareComponent.tsx
+++ src/auth/AuthMiddlewareComponent.tsx
@@ -1,13 +1,31 @@
+import { gql, useQuery } from '@apollo/client'
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { userStore } from '../stores/userStore'
+import { UserType } from '../types'
 
 interface Props {}
 
+const ME_QUERY = gql`
+    query Query {
+        me {
+            _id
+            username
+            email
+            avatar
+        }
+    }
+`
 const AuthMiddleware: React.FC<Props> = ({ children }) => {
-    const hehe = userStore(state => state.user)
-    console.log(hehe)
-    if (hehe === null) return <Navigate to="/login" replace />
+    const { data, loading } = useQuery<{ me: UserType }>(ME_QUERY)
+    const store = userStore(state => state)
+    console.log(store)
+    if (loading) return <div>Loading</div>
+    if (store.user === null) {
+        console.log(data)
+        if (!data) return <Navigate to="/login" replace />
+        store.setUser(data.me)
+    }
     return <>{children}</>
 }
 
