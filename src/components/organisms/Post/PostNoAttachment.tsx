1b5d65b2065285f03ea4c3785f46eee8b9d74c77
Syncxv <aruldevarackal1@gmail.com>
2022-02-12 19:58:23 +0530

    make them both align eh

--- src/components/organisms/Post/PostNoAttachment.tsx
+++ src/components/organisms/Post/PostNoAttachment.tsx
@@ -14,19 +14,18 @@ const PostNoAttachment: React.FC<Props> = ({ post }) => {
     const naviagtor = useNavigate()
     return (
         <>
-            <article onClick={() => naviagtor(`/post/${post._id}`)} className={PST.postTextOnly}>
-                <div className={PST.postTextOnly__contentWrapper}>
-                    <div className={PST.postTextOnly__avatar}>
-                        <img src={post.owner.avatar || deafultPfp} alt="" />
-                    </div>
-                    <div className={PST.postTextOnly__content}>
-                        <div className={PST.userInfo}>
-                            <div className={PST.username}>{post.owner.username}</div>
-                        </div>
-                        <div className={PST.postTextOnly__text}>{post.content}</div>
-                    </div>
+            <article onClick={() => naviagtor(`/post/${post._id}`)} className={PST.postv2}>
+                <div className={PST.avatarWrapper}>
+                    <img className={PST.avatar} src={post.owner.avatar || deafultPfp} alt="" />
                 </div>
-                <Actions post={post} />
+                <div className={PST.content}>
+                    <div className={PST.owner}>
+                        <h4>{post.owner.username}</h4>
+                        <span>@{post.owner.username}</span>
+                    </div>
+                    <div>{post.content}</div>
+                    <Actions post={post} />
+                </div>
             </article>
         </>
     )
